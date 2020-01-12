f0a17cde10cab46b07c616abb716c55e47d634ef
severo cruz <severo_86@hotmail.com>
2020-01-11 20:27:45 -0400

    modificaciones para iphone x o mas

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts('js/sw-utils.js');
-const STATIC_CACHE    = 'static-v3';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -40,9 +40,16 @@ self.addEventListener('activate', e => {
 
     const respuesta_act = caches.keys().then(keys =>{
         keys.forEach( key =>{
+            
             if(key !== STATIC_CACHE && key.includes('static')){
                 return caches.delete(key);
             }
+            
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
+                return caches.delete(key);
+            }
+
+
         });
     });
    e.waitUntil(respuesta_act);
